bf6ecf21f98396de5df391b029dda289db3bb2a8
sgosiaco <sgosiaco@me.com>
2016-05-15 03:53:27 -0700

    Style changes

--- vector.cpp
+++ vector.cpp
@@ -7,13 +7,13 @@ Vector<T>::Vector()
 {
   storage = count = 0;
   array = NULL;
-}
+} //con
 
 template <class T>
 Vector<T>::~Vector()
 {
   delete [] array;
-}
+} //decon
 
 template <class T>
 Vector<T>::Vector(const T arr[])
@@ -24,7 +24,7 @@ Vector<T>::Vector(const T arr[])
 
   for(int i = 0; i < storage; i++)
     array[i] = arr[i];
-}
+} //array con
 
 template <class T>
 Vector<T>& Vector<T>::operator=(const Vector<T>& vec)
@@ -34,6 +34,7 @@ Vector<T>& Vector<T>::operator=(const Vector<T>& vec)
 
   count = vec.count;
   storage = vec.storage;
+
   if(array)
     delete [] array;
 
@@ -43,7 +44,7 @@ Vector<T>& Vector<T>::operator=(const Vector<T>& vec)
     array[i] = vec.array[i];
 
   return *this;
-}
+} //=
 
 template <class T>
 Vector<T>& Vector<T>::operator+=(const Vector<T>& vec)
@@ -52,10 +53,11 @@ Vector<T>& Vector<T>::operator+=(const Vector<T>& vec)
   {
     if((count + vec.count) < (2 * storage))
       storage *= 2;
-    else
+    else //exact
       storage = count + vec.count;
     T* temp = array;
     array = new T[storage];
+
     for(int i = 0; i < count; i++)
       array[i] = temp[i];
 
@@ -64,15 +66,16 @@ Vector<T>& Vector<T>::operator+=(const Vector<T>& vec)
 
     delete [] temp;
     count += vec.count;
-  }
-  else
+  } //if
+  else //no resize
   {
     for(int i = 0; i < vec.count; i++)
       array[i + count] = vec.array[i];
     count += vec.count;
-  }
+  } //else
+
   return *this;
-}
+} //+=
 
 template <class T>
 size_t Vector<T>::find(const Vector<T> in) const
@@ -84,27 +87,31 @@ size_t Vector<T>::find(const Vector<T> in) const
     if(array[i] == in.array[0])
     {
       found = 1;
+
       for(int j = 0; j < insize; j++)
         if(array[i + j] != in.array[j])
           found = 0;
+
       if(found)
         return i;
-    }
-  }
+    } //if
+  } //for
+
   return (size_t)-1;
-}
+} //find()
 
 template <class T>
 Vector<T> Vector<T>::substr(int start, int length)
 {
   Vector<T> temp;
   temp.array = new T[length];
+
   for(int i = 0; i < length; i++)
     temp.array[i] = array[i + start];
 
   temp.count = temp.storage = length;
   return temp;
-}
+} //substr()
 
 template <class T>
 Vector<T>& Vector<T>::insert(int start, const Vector<T> in)
@@ -115,24 +122,29 @@ Vector<T>& Vector<T>::insert(int start, const Vector<T> in)
       storage *= 2;
     T* temp = array;
     array = new T[storage];
+
     for(int i = 0; i < start + 1; i++)
       array[i] = temp[i];
+
     for(int i = 0; i < in.count; i++)
       array[start + i] = in.array[i];
+
     for(int i = 0; i < count - start; i++)
       array[start + in.count + i] = temp[start + i];
     delete [] temp;
-  }
-  else
+  } //if
+  else //no resize
   {
     for(int i = storage - 1; i > start; i--)
       array[i] = array[i - in.count];
+
     for(int i = start; i < in.count + start; i++)
       array[i] = in.array[i - start];
-  }
+  } //else
+
   count += in.count;
   return *this;
-}
+} //insert()
 
 template <class T>
 Vector<T>& Vector<T>::replace(int st, int len, Vector<T> in, int sst, int slen)
@@ -141,33 +153,36 @@ Vector<T>& Vector<T>::replace(int st, int len, Vector<T> in, int sst, int slen)
   {
     if((count - len + slen) < (2 * storage))
       storage *= 2;
-    else
+    else //not more than double
       storage = count - len + slen;
     T* temp = array;
     array = new T[storage];
+
     for(int i = 0; i < count; i++)
       array[i] = temp[i];
     delete [] temp;
-  }
+  } //if
+
   for(int i = count - st - len; i > 0; i--)
     array[st + slen + i - 1] = array[st + len + i - 1];
+
   for(int i = 0; i < slen; i++)
     array[st + i] = in.array[i + sst];
   count += (slen - len);
   return *this;
-}
+} //replace()
 
 template <class T>
 int Vector<T>::size() const
 {
   return count;
-}
+} //size()
 
 template <class T>
 int Vector<T>::capacity() const
 {
   return storage;
-}
+} //capacity()
 
 template <class T>
 ostream& operator<<(ostream& os, const Vector<T>& vec)
@@ -175,4 +190,4 @@ ostream& operator<<(ostream& os, const Vector<T>& vec)
   for(int i = 0; i < vec.count; i++)
       os << vec.array[i];
   return os;
-}
+} //<<
